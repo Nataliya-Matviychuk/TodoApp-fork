f4c9c7db2dc120f35b2649f3a33018a55c6fad11
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2025-02-23 22:29:15 +0100

    add calendar into home page

---
+++ todo_list/static/js/calendar.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let currentMonth = date.getMonth();
+    let currentDay = date.getDate();
+    let daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+    let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+    let tableForMonth = document.createElement('table');
+    let header = document.createElement('tr');
+
+    for (let i = 0; i < 7; i++) {
+        let cell = document.createElement('th');
+        cell.textContent = daysOfWeek[i];
+        header.appendChild(cell);
+    }
+
+    tableForMonth.appendChild(header);
+
+    for (let i = 1; i <= 42; i++) {
+
+        if (i % 7 === 1) {
+            var row = document.createElement('tr');
+            tableForMonth.appendChild(row);
+        }
+
+        let cell = document.createElement('td');
+
+        if (i >= firstDayOfMonth && i < daysInMonth + firstDayOfMonth) {
+            cell.textContent = i - firstDayOfMonth + 1;
+            if (cell.textContent == currentDay) {
+                cell.style.backgroundColor = "#ee394c";
+                cell.style.color = '#ffffff';
+            }
+        } else {
+            let prevMonthDays = new Date(currentYear, currentMonth, 0).getDate();
+            if (i < firstDayOfMonth) {
+                cell.textContent = prevMonthDays - (firstDayOfMonth - i - 1);
+                cell.style.backgroundColor = 'rgba(187, 233, 219, 0.5)';
+            } else {
+                cell.textContent = i - daysInMonth - firstDayOfMonth + 1;
+                cell.style.backgroundColor = 'rgba(187, 233, 219, 0.5)';
+            }
+        }
+
+        row.appendChild(cell);
+    }
+
+    let currentMonthYear = document.createElement('h1');
+    currentMonthYear.setAttribute('class', 'calendar-heading');
+    currentMonthYear.textContent = `${months[currentMonth]}  ${currentYear}`;
+
+    let calendarContainer = document.createElement('div');
+    calendarContainer.setAttribute('class', 'calendar-container');
+    calendarContainer.appendChild(currentMonthYear);
+    calendarContainer.appendChild(tableForMonth);
+
+    let heading = document.createElement('h1');
+    heading.textContent = "Your Calendar";
+
+    let paragraphOne = document.createElement('p');
+    paragraphOne.textContent = 'Our calendar helps you to manage your tasks and events.';
+
+    let paragraphTwo = document.createElement('p');
+    paragraphTwo.textContent = 'It is easy to use and you can see your tasks and events in a glance.';
+
+    let featureContent = document.createElement('div');
+    featureContent.setAttribute('class', 'feature-content');
+    featureContent.appendChild(heading);
+    featureContent.appendChild(paragraphOne); 
+    featureContent.appendChild(paragraphTwo);
+
+    let calendarElement = document.getElementById('calendar');
+    calendarElement.appendChild(calendarContainer);
+    calendarElement.appendChild(featureContent);
+});
